2684cfb33f4b7f95a3218bb81c33a880e20d29b5
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-08-16 20:12:04 +0200

    Finished first class

---
+++ WebApplication1/WebApplication1/Controllers/EmptyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class EmptyController : ApiController
+    {
+        // GET: api/HelloWorld
+        //[HttpGet]
+        //TODO redefine the routes in WebApiConfig.cs so the methods could be called
+        public string GetHelloWorld()
+        {
+            return "Hello world";
+        }
+        
+        public string GetGoodByeWorld()
+        {
+            return "Good Bye World";
+        }
+    }
+}
