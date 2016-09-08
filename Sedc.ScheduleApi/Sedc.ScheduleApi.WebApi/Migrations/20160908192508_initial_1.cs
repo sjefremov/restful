a084205894ffc1fbbfd710e8bf7cc0a44bfb2ec5
SJEFREMOV\Стојанчо <stojanco.jefremov@gmail.com>
2016-09-08 22:41:42 +0200

    Some changes done so the project is working now...

---
+++ Sedc.ScheduleApi/Sedc.ScheduleApi.WebApi/Migrations/20160908192508_initial_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sedc.ScheduleApi.WebApi.Migrations
+{
+    public partial class initial_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
